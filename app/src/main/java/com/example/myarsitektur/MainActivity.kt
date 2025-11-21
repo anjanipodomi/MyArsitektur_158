28caea95368086cc241ae4a17d33edeec46143ac
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-21 16:32:44 +0700

    menambahkan import di MainActivity

--- app/src/main/java/com/example/myarsitektur/MainActivity.kt
+++ app/src/main/java/com/example/myarsitektur/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.myarsitektur
 
-
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.myarsitektur.ui.theme.MyArsitekturTheme
+import com.example.myarsitektur.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
