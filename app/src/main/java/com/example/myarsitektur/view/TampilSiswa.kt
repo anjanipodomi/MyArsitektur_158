9693b8eb5940d08adfa7581c877e39d6b7b6a8b7
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-21 16:08:59 +0700

    Menambahkan fungsi `TampilSiswa` sebagai komponen UI untuk menampilkan data siswa dengan parameter statusUISiswa dan aksi tombol kembali

--- app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
@@ -1,42 +1,9 @@
 package com.example.myarsitektur.view
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.unit.dp
-import com.example.navigasiku.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TampilData(
-    onBackBtnClick: () -> Unit
-) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "Data Diri", color = Color.White) },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = colorResource(id = R.color.teal_700)
-                )
-            )
-        }
-    ) { isiRuang ->
-        Column(
-            modifier = Modifier
-                .padding(isiRuang)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Data berhasil dikirim!", color = Color.Black)
-            Spacer(modifier = Modifier.height(30.dp))
-            Button(onClick = onBackBtnClick) {
-                Text("Kembali ke Formulir")
-            }
-        }
-    }
-}
+fun TampilSiswa(
+    statusUISiswa: Siswa,
+    onBackButtonClicked: () -> Unit
+)
