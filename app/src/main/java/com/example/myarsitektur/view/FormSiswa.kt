215ad9f53564f9b1e16a8c9907e8530969b5514e
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-21 16:06:19 +0700

    Menambahkan import yang dibutuhkan untuk Compose UI, termasuk layout, TopAppBar, Material3, state, color, string resources, dan unit dp

--- app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
@@ -1,5 +1,19 @@
 package com.example.myarsitektur.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.myarsitektur.R
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormSiswa(
